8347485e0744a00c3a6421bf11b8360410fe6d91
Rahul Arora <rahulxxarora@gmail.com>
2017-09-04 01:00:37 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,95 @@
+Custom REST-API Server
+======================
+
+
+Setting up the environment
+--------------------------
+
+```bash
+sudo pip install -r requirements.txt
+```
+
+Running tests
+-------------
+
+```bash
+python tests.py
+```
+
+Running the application
+-----------------------
+
+```bash
+python app.py
+```
+
+API Docs
+--------
+
+### Fetching all products
+
+@api {get} /products
+
+Example:
+
+```bash
+$ curl -H 'Authorization: Basic cm9vdDpwd2Q=' -H 'user: root' https://gentle-cliffs-92685.herokuapp.com/products
+```
+
+### Fetching a particular product
+
+@api {get} /products/<product ID>
+
+Example:
+
+```bash
+$ curl -H 'Authorization: Basic cm9vdDpwd2Q=' -H 'user: root' https://gentle-cliffs-92685.herokuapp.com/products/foo
+```
+
+### Creating a new user
+
+@api {post} /user
+
+@apiParam {String, String} user and pwd
+
+@apiOutput {String} AuthToken
+
+Example:
+
+```bash
+$ curl -d '{"user":"test", "pwd":"test"}' https://gentle-cliffs-92685.herokuapp.com/user
+```
+
+### Adding a new product
+
+@api {post} /products
+
+@apiParam {String, String, String} id, seller and price
+
+Example:
+
+```bash
+$ curl -H 'Authorization: Basic cm9vdDpwd2Q=' -H 'user: root' -d '{"id":"bar", "seller":"foo", "price":"149"}' https://gentle-cliffs-92685.herokuapp.com/products
+```
+
+### Updating a product
+
+@api {put} /products
+
+@apiParam {String, String, String} id, seller and price
+
+Example:
+
+```bash
+$ curl -X PUT -H 'Authorization: Basic cm9vdDpwd2Q=' -H 'user: root' -d '{"id":"bar", "seller":"foo", "price":"249"}' https://gentle-cliffs-92685.herokuapp.com/products
+```
+
+### Deleting a product
+
+@api {delete} /products/<product ID>
+
+Example:
+
+```bash
+$ curl -X DELETE -H 'Authorization: Basic cm9vdDpwd2Q=' -H 'user: root' https://gentle-cliffs-92685.herokuapp.com/products/foo
+```
---
+++ tests.py
@@ -0,0 +1,127 @@
+import json
+import unittest
+from multiprocessing import Process
+
+import requests
+
+from app import run
+
+headers = {'Authorization': 'Basic cm9vdDpwd2Q=', 'user': 'root'}
+
+
+class APITestSuit(unittest.TestCase):
+    def test_auth_invalid_request(self):
+        _headers = {'NotAuthorization': 'Basic cm9vdDpwd2Q=', 'user': 'root'}
+        response = requests.get('http://127.0.0.1:8000/products',
+                                headers=_headers)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json.loads(response.text), {'msg': 'Invalid header'})
+
+    def test_auth_unauthorized_request(self):
+        _headers = {'Authorization': 'Basic cm9vdDpwd2Q=', 'user': 'Notroot'}
+        response = requests.get('http://127.0.0.1:8000/products',
+                                headers=_headers)
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            json.loads(response.text), {'msg': 'Unauthorized request'})
+
+    def test_get_all_products(self):
+        response = requests.get('http://127.0.0.1:8000/products',
+                                headers=headers)
+        self.assertEqual(response.status_code, 200)
+        jsonObj = json.loads(response.text)
+        self.assertEqual(jsonObj['foo'],
+                         {'id': 'foo',
+                          'seller': 'bar',
+                          'price': '99'})
+
+    def test_get_item_not_found(self):
+        response = requests.get('http://127.0.0.1:8000/products/test',
+                                headers=headers)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(json.loads(response.text), {'msg': 'Item not found'})
+
+    def test_get_invalid_url(self):
+        response = requests.get('http://127.0.0.1:8000/products/test/test1',
+                                headers=headers)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json.loads(response.text), {'msg': 'Invalid endpoint'})
+
+    def test_post_product(self):
+        response = requests.post(
+            'http://127.0.0.1:8000/products',
+            headers=headers,
+            json={'id': 'bar',
+                  'seller': 'foo',
+                  'price': '49'})
+        self.assertEqual(response.status_code, 201)
+        response = requests.get('http://127.0.0.1:8000/products/bar',
+                                headers=headers)
+        self.assertEqual(
+            json.loads(response.text),
+            {'price': '49',
+             'id': 'bar',
+             'seller': 'foo'})
+
+    def test_delete_product(self):
+        requests.post(
+            'http://127.0.0.1:8000/products',
+            headers=headers,
+            json={'id': 'bar',
+                  'seller': 'foo',
+                  'price': '49'})
+        response = requests.delete(
+            'http://127.0.0.1:8000/products/bar', headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            json.loads(response.text), {'msg': 'Item deleted successfully'})
+
+    def test_delete_item_not_found(self):
+        response = requests.delete(
+            'http://127.0.0.1:8000/products/test', headers=headers)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(json.loads(response.text), {'msg': 'Item not found'})
+
+    def test_delete_invalid_url(self):
+        response = requests.delete(
+            'http://127.0.0.1:8000/products/test/test1', headers=headers)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.text), {'msg': 'Invalid endpoint'})
+
+    def test_put_product(self):
+        requests.post(
+            'http://127.0.0.1:8000/products',
+            headers=headers,
+            json={'id': 'bar',
+                  'seller': 'foo',
+                  'price': '49'})
+        response = requests.get('http://127.0.0.1:8000/products/bar',
+                                headers=headers)
+        self.assertEqual(
+            json.loads(response.text),
+            {'price': '49',
+             'id': 'bar',
+             'seller': 'foo'})
+        requests.post(
+            'http://127.0.0.1:8000/products',
+            headers=headers,
+            json={'id': 'bar',
+                  'seller': 'foo',
+                  'price': '109'})
+        response = requests.get('http://127.0.0.1:8000/products/bar',
+                                headers=headers)
+        self.assertEqual(
+            json.loads(response.text),
+            {'price': '109',
+             'id': 'bar',
+             'seller': 'foo'})
+
+
+if __name__ == '__main__':
+    p1 = Process(target=run)
+    p1.start()
+    p2 = Process(target=unittest.main)
+    p2.start()
+    p2.join()
+    p1.terminate()
